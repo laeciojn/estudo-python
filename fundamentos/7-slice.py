df9cad393561ad0999fa7a581d08b32d0ba1b0ae
Laecio Jesus <laeciojesusn@gmail.com>
2025-12-12 15:30:17 -0300

    Remove unused files and update comments in 7-slice.py

--- fundamentos/7-slice.py
+++ fundamentos/7-slice.py
@@ -1,3 +1,6 @@
 movieName = "Top Gun"
 
-# string[inicio:fim] - indice começa na posição 
+# string[inicio:fim] - indice começa na posição 0 | índice final -1
+
+# 1 - Buscar toda string a partir da primeira posição
+print(movieName)
