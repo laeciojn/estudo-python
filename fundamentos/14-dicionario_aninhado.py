287b9a07a581a767827f8f611b7c54e47f029abd
Laecio Jesus <laeciojesusn@gmail.com>
2025-12-24 19:11:32 -0300

    Add 14-dicionario_aninhado.py with nested dictionary example

---
+++ fundamentos/14-dicionario_aninhado.py
@@ -0,0 +1,14 @@
+filmsDict = {
+  "inception": {
+    "yearRealease" : 2010,
+    "imdbRating": 8.8,
+    "genre": ["Sci-fi", "Action", "Thriller"]
+  },
+  "interstellar" :{
+    "yearRealease" : 2010,
+    "imdbRating": 8.8,
+    "genre": ["Sci-fi", "Action", "Thriller"]
+  }
+}
+
+print(filmsDict)
